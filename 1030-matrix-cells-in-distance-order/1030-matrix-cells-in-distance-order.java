e306643da8a6e77e7f2aae9f0aa2cf81a21dd529
Atul Sharma <112771777+atulsharma3011@users.noreply.github.com>
2022-09-23 17:14:47 +0530

    Time: 13 ms (90.16%), Space: 44.1 MB (97.93%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] ans=new int[rows*cols][2];
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                int boxno=i*cols+j;
+                ans[boxno][0]=i;
+                ans[boxno][1]=j;
+            }
+        }
+        Arrays.sort(ans,(a,b)->{
+            int d1=Math.abs(a[0]-rCenter)+Math.abs(a[1]-cCenter);
+            int d2=Math.abs(b[0]-rCenter)+Math.abs(b[1]-cCenter);
+            return d1-d2;
+        });
+        return ans;
+    }
+}
