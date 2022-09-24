7edce4e4b84ff1009c02928ae1360016b747a851
Atul Sharma <112771777+atulsharma3011@users.noreply.github.com>
2022-09-24 21:53:32 +0530

    Time: 1 ms (92.19%), Space: 41.7 MB (80.52%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder ans=new StringBuilder();
+        int i=0;
+        while(i<command.length())
+        {
+            if(command.charAt(i)=='G'){
+                ans.append("G");
+                i++;
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                ans.append("o");
+                i=i+2;
+            }
+            else{
+                ans.append("al");
+                i=i+4;
+            }
+        }
+        return ans.toString();
+    }
+}
